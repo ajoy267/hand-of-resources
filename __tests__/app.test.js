715c832babbfe74774f8ec5d91d88a7052a092c0
ajoy267 <andrew.joy12@hotmail.com>
2022-03-17 09:11:49 -0700

    wrote my getall test and controller and model and got my test to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Bike = require('../lib/models/Bike');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -20,5 +21,11 @@ describe('hand-of-resources routes', () => {
     };
     const res = await request(app).post('/api/v1/bikes').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets all bikes', async () => {
+    const expected = await Bike.getAll();
+    const res = await request(app).get('/api/v1/bikes');
+    expect(res.body).toEqual(expected);
   });
 });
