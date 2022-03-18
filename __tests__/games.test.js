dc1bcb79dacade4d20605e4548c58b8b361e642b
ajoy267 <andrew.joy12@hotmail.com>
2022-03-18 10:17:41 -0700

    wrote my get all

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Game = require('../lib/models/Game');
 
 describe('car routes', () => {
   beforeEach(() => {
@@ -19,5 +20,11 @@ describe('car routes', () => {
     };
     const res = await request(app).post('/api/v1/games').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets all games', async () => {
+    const expected = await Game.getAll();
+    const res = await request(app).get('/api/v1/games');
+    expect(res.body).toEqual(expected);
   });
 });
